10afc4ea1cbeaaeab21d98cd673d27720896a8ad
Adam Hyben <128488292+Habybyi@users.noreply.github.com>
2026-05-31 18:53:31 +0200

    Refactor dungeon crawler game logic and map generation

--- dungeon-crawler/Program.cs
+++ dungeon-crawler/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,53 +6,107 @@ namespace DungeonCrawler
 {
     class Program
     {
-        // Hráč a jeho vlastnosti
-        static int playerX = 1, playerY = 1;
+        static int mapWidth = 15;
+        static int mapHeight = 8;
+        static char[,] map;
+
+        static int playerX, playerY;
         static int hp = 100;
+        static int maxHp = 100;
+        static int baseDamage = 10;
         static List<Item> inventory = new List<Item>();
 
-        // Mapa ( # = stena, . = cesta, E = nepriateľ, I = item )
-        static string[] map = {
-            "##########",
-            "#........#",
-            "#.I..E...#",
-            "#........#",
-            "#.E....I.#",
-            "##########"
-        };
+
+        static Random rand = new Random();
 
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
+            
+            GenerateRandomMap();
+
             while (hp > 0)
             {
                 Draw();
                 HandleInput();
             }
+
             Console.Clear();
-            Console.WriteLine("Game Over! Zomrel si v dungeone.");
+            Console.WriteLine("========================================");
+            Console.WriteLine("  GAME OVER! Zomrel si v dungeone.       ");
+            Console.WriteLine("========================================");
+        }
+
+        static void GenerateRandomMap()
+        {
+            map = new char[mapHeight, mapWidth];
+
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    if (y == 0 || y == mapHeight - 1 || x == 0 || x == mapWidth - 1)
+                    {
+                        map[y, x] = '#';
+                    }
+                    else
+                    {
+                        int chance = rand.Next(100);
+
+                        if (chance < 8) map[y, x] = 'E';       
+                        else if (chance < 13) map[y, x] = 'I'; 
+                        else if (chance < 18) map[y, x] = '#'; 
+                        else map[y, x] = '.';                 
+                    }
+                }
+            }
+
+          
+            do
+            {
+                playerX = rand.Next(1, mapWidth - 1);
+                playerY = rand.Next(1, mapHeight - 1);
+            } while (map[playerY, playerX] != '.');
         }
 
         static void Draw()
         {
             Console.SetCursorPosition(0, 0);
-            Console.WriteLine("--- DUNGEON CRAWLER ---");
-            
-            // Výpočet bonusov z inventára
+            Console.WriteLine("=== NÁHODNÝ DUNGEON CRAWLER ===");
+
             int damageBonus = inventory.Sum(i => i.DamageBoost);
-            
-            for (int y = 0; y < map.Length; y++)
+            int defenseBonus = inventory.Sum(i => i.DefenseBoost);
+
+            for (int y = 0; y < mapHeight; y++)
             {
-                for (int x = 0; x < map[y].Length; x++)
+                for (int x = 0; x < mapWidth; x++)
                 {
-                    if (x == playerX && y == playerY) Console.Write("@"); // Hráč
-                    else Console.Write(map[y][x]);
+                    if (x == playerX && y == playerY) 
+                        Console.Write("@");
+                    else 
+                        Console.Write(map[y, x]);
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("\n--- TVOJE STATY ---");
+            Console.WriteLine($"HP: {hp}/{maxHp} | Útok: {baseDamage + damageBonus} (+{damageBonus}) | Obrana: {defenseBonus}");
+            
+            Console.WriteLine("\n--- INVENTÁR ---");
+            if (inventory.Count == 0)
+            {
+                Console.WriteLine("Prázdny");
+            }
+            else
+            {
+                foreach (var item in inventory)
+                {
+                    string stats = "";
+                    if (item.DamageBoost > 0) stats += $"[Útok +{item.DamageBoost}] ";
+                    if (item.DefenseBoost > 0) stats += $"[Obrana +{item.DefenseBoost}] ";
+                    Console.WriteLine($"- {item.Name} {stats}");
+                }
+            }
 
-            Console.WriteLine($"\nHP: {hp} | Sila: {10 + damageBonus}");
-            Console.WriteLine("Inventár: " + (inventory.Count == 0 ? "Prázdny" : string.Join(", ", inventory.Select(i => i.Name))));
             Console.WriteLine("\nPohyb: WASD | Koniec: Q");
         }
 
@@ -67,44 +121,83 @@ namespace DungeonCrawler
             if (key == ConsoleKey.D) newX++;
             if (key == ConsoleKey.Q) Environment.Exit(0);
 
-            char target = map[newY][newX];
+            char target = map[newY, newX];
 
-            if (target == '#') return; // Stena, nepohneme sa
+            if (target == '#') return; 
 
             if (target == 'E') {
                 Fight();
-                RemoveObject(newX, newY);
+                map[newY, newX] = '.'; 
             }
             else if (target == 'I') {
-                PickUpItem();
-                RemoveObject(newX, newY);
+                PickUpRandomItem();
+                map[newY, newX] = '.';
             }
 
             playerX = newX;
             playerY = newY;
         }
 
-        static void PickUpItem()
+        static void PickUpRandomItem()
         {
-            var sword = new Item { Name = "Meč", DamageBoost = 5 };
-            inventory.Add(sword);
+            string[] weapons = { "Hrdzavý meč", "Oceľová dýka", "Bojová sekera", "Palcát skazy" };
+            string[] armors = { "Kožená vesta", "Krúžkové brnenie", "Plátový pancier", "Mystický plášť" };
+            string[] consumables = { "Malý liečivý lektvar", "Veľký liečivý lektvar" };
+
+            int itemType = rand.Next(3); 
+            Item newItem = null;
+
+            Console.Clear();
+
+            if (itemType == 0) 
+            {
+                newItem = new Item {
+                    Name = weapons[rand.Next(weapons.Length)],
+                    DamageBoost = rand.Next(3, 10),
+                    DefenseBoost = 0
+                };
+                inventory.Add(newItem);
+                Console.WriteLine($"Našiel si zbraň: {newItem.Name} (Útok +{newItem.DamageBoost})!");
+            }
+            else if (itemType == 1) 
+            {
+                newItem = new Item {
+                    Name = armors[rand.Next(armors.Length)],
+                    DamageBoost = 0,
+                    DefenseBoost = rand.Next(2, 6) 
+                };
+                inventory.Add(newItem);
+                Console.WriteLine($"Našiel si zbroj: {newItem.Name} (Obrana +{newItem.DefenseBoost})!");
+            }
+            else 
+            {
+                int healAmount = rand.Next(15, 35);
+                hp = Math.Min(maxHp, hp + healAmount);
+                Console.WriteLine($"Našiel si a vypil: {consumables[rand.Next(consumables.Length)]}! Vyliečil si si {healAmount} HP.");
+            }
+
+            Console.WriteLine("\nStlač ľubovoľný kláves pre pokračovanie...");
+            Console.ReadKey();
         }
 
         static void Fight()
         {
-            int damageDealt = 10 + inventory.Sum(i => i.DamageBoost);
-            hp -= 20; // Nepriateľ ti vždy uberie 20 HP
-            Console.Clear();
-            Console.WriteLine($"Bojoval si! Spôsobil si {damageDealt} dmg a stratil si 20 HP.");
-            Console.WriteLine("Stlač ľubovoľný kláves...");
-            Console.ReadKey();
-        }
+            int damageBonus = inventory.Sum(i => i.DamageBoost);
+            int defenseBonus = inventory.Sum(i => i.DefenseBoost);
 
-        static void RemoveObject(int x, int y)
-        {
-            char[] row = map[y].ToCharArray();
-            row[x] = '.';
-            map[y] = new string(row);
+            int playerDamage = baseDamage + damageBonus;
+
+            int enemyDamage = Math.Max(2, 20 - defenseBonus); 
+            
+            hp -= enemyDamage;
+
+            Console.Clear();
+            Console.WriteLine("⚔️ === BOJ === ⚔️");
+            Console.WriteLine($"Zaútočil si na nepriateľa za {playerDamage} dmg.");
+            Console.WriteLine($"Nepriateľ ti vrátil úder. Vďaka tvojej obrane (+{defenseBonus}) ti ubral iba {enemyDamage} HP.");
+            
+            Console.WriteLine("\nStlač ľubovoľný kláves...");
+            Console.ReadKey();
         }
     }
 
@@ -112,5 +205,6 @@ namespace DungeonCrawler
     {
         public string Name { get; set; }
         public int DamageBoost { get; set; }
+        public int DefenseBoost { get; set; }
     }
 }
